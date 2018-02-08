eceff4e4505ef127d0efefb7e002a8fad7381d66
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2018-02-08 08:09:18 +0100

    String Reverser

--- Projects/FizzBuzz/Program.cs
+++ Projects/FizzBuzz/Program.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <summary>
-//   Task Fizz Buzz
+//   Fizz Buzz
 //   Write a program that prints the numbers from 1 to 100. 
 //   But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
 //   For numbers which are multiples of both three and five print “FizzBuzz”.
@@ -20,10 +20,7 @@ namespace FizzBuzz
         /// <summary>
         /// The main.
         /// </summary>
-        /// <param name="args">
-        /// The args.
-        /// </param>
-        private static void Main(string[] args)
+        private static void Main()
         {
             for (var i = 1; i <= 100; i++)
             {
---
+++ Projects/String Reverser/Program.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//   Reverse a String
+//   Enter a string and the program will reverse it and print it out.
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace String_Reverser
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        private static void Main()
+        {
+            var input = Console.ReadLine();
+            var output = new string(input?.ToCharArray().Reverse().ToArray());
+            Console.WriteLine(output);
+        }
+    }
+}
