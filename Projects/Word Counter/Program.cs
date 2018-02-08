c524ec0221de8e13cd918f4d0267543227d4e85f
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2018-02-08 09:00:15 +0100

    Word Counter

---
+++ Projects/Word Counter/Program.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//   Count Words in a String
+//   Counts the number of individual words in a string.
+//   For added complexity read these strings in from a text file and generate a summary.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Word_Counter
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        private static void Main()
+        {
+            var input = Console.ReadLine();
+            var delimiters = new[] { ' ', ',', '.' };
+            Console.WriteLine(input?.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length);
+        }
+    }
+}
