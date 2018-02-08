4ee326221bb0728a174743fe825a6db6490b4d78
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2018-02-08 08:44:12 +0100

    Vowel Counter

---
+++ Projects/Count Vowels/Program.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//   Count Vowels
+//   Enter a string and the program counts the number of vowels in the text.
+//   For added complexity have it report a sum of each vowel found.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Count_Vowels
+{
+    using System;
+
+    /// <summary>
+    ///     The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        ///     The main.
+        /// </summary>
+        private static void Main()
+        {
+            var input = Console.ReadLine();
+            var arr = input?.ToCharArray();
+
+            var counterA = 0;
+            var counterE = 0;
+            var counterI = 0;
+            var counterO = 0;
+            var counterU = 0;
+
+            if (arr == null)
+            {
+                return;
+            }
+
+            foreach (var letter in arr)
+            {
+                switch (letter)
+                {
+                    case 'a':
+                        counterA++;
+                        break;
+                    case 'e':
+                        counterE++;
+                        break;
+                    case 'i':
+                        counterI++;
+                        break;
+                    case 'o':
+                        counterO++;
+                        break;
+                    case 'u':
+                        counterU++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(
+                "Vowels amount:" + "\nA: " + counterA + "\nE: " + counterE + "\nI: " + counterI + "\nO: " + counterO
+                + "\nU: " + counterU);
+        }
+    }
+}
