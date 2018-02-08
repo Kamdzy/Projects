484a4055dcec231e85271aaa32fc76a7ae8d50cb
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2018-02-08 07:51:47 +0100

    Fizz Buzz

---
+++ Projects/FizzBuzz/Program.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//   Task Fizz Buzz
+//   Write a program that prints the numbers from 1 to 100. 
+//   But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+//   For numbers which are multiples of both three and five print “FizzBuzz”.
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace FizzBuzz
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write(i % 5 == 0 ? " Fizz-Buzz " : " Fizz ");
+                }
+                else
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.Write(" Buzz ");
+                    }
+                    else
+                    {
+                        Console.Write(" " + i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
